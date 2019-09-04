21b6c8ec1a6e0415f1bb37e38a9b4d6f9a5808ca
Thomas Taylor <thomas.taylor@kineo.com>
2019-09-04 20:41:07 +0100

    Latest updates

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const fs = require('fs-extra');
 const origin = require('../../lib/application')();
 const path = require('path');
 const server = module.exports = express();
+/** @todo should be in the DB **/
+const currentExports = {};
 
 let adapt, course;
 
@@ -17,26 +19,17 @@ function init() {
   origin.contentmanager.getContentPlugin('course', (error, plugin) => {
     if (error) return console.log(error);
     course = plugin;
-    course.update({}, {}, (error, results) => {
-    // course.update({ _id: "5ce3fc6ce9beec8458807b6a" }, { exportedAt: new Date() }, (error, results) => {
-      console.log(error, results);
-    });
   });
+
+  origin.db.addModel('courseexport', { courseId: 'objectid', time: 'date' });
 
   server.set('views', __dirname);
   server.set('view engine', 'hbs');
   // routes
   server.get('/bulkexport', renderIndex);
   server.post('/bulkexport', bulkExport);
-
-  const customSchema = {
-    exportedAt: {
-      type: "date",
-      required: false,
-      editorOnly: true
-    }
-  };
-  origin.db.getModel('course').schema.add(customSchema);
+  server.get('/bulkexport/download/:id', sendExports);
+  server.get('/bulkexport/poll/:id', pollExportProgress);
 }
 
 function renderIndex(req, res, next) {
@@ -51,28 +44,68 @@ function bulkExport(req, res, next) {
     return next('Must provide courses to export!');
   }
   const exportsRoot = path.join(origin.configuration.tempDir, origin.configuration.getConfig('masterTenantID'), Constants.Folders.Exports, `Export ${new Date().toISOString()}`);
+  const id = new Date().getTime();
+
+  currentExports[id] = {
+    dir: exportsRoot,
+    completed: 0,
+    total: req.body.courses.length, errors: []
+  };
+  req.params.id = id;
+  pollExportProgress(req, res, next);
+
   fs.ensureDirSync(exportsRoot);
 
   async.eachSeries(req.body.courses, (courseId, cb) => {
+    const handleError = (error) => {
+      console.log(error);
+      currentExports[id].errors.push({ courseId, error });
+      cb();
+    };
     course.retrieve({ _id: courseId }, {}, (error, results) => {
-      if(error || !results.length) return cb(error);
+      if(error) {
+        return handleError(error);
+      }
+      if(!results.length) {
+        return handleError(new Error(`No course with _id '${courseId}'`));
+      }
       adapt.export(courseId, { outputdir: path.join(exportsRoot, results[0].title) }, error => {
-        if(error) return cb(error);
-        course.update({ _id: courseId }, { exportedAt: new Date() }, cb);
+        if(error) return handleError(error);
+
+        origin.db.create('courseexport', { courseId: courseId, time: new Date() }, error => {
+          if(error) return handleError(error);
+
+          currentExports[id].completed++;
+          cb();
+        });
       });
     });
-  }, error => {
-    if(error) {
-      return next(error);
-    }
-    const output = fs.createWriteStream(`${exportsRoot}.zip`);
-    output.on('close', () => { fs.remove(exportsRoot); });
-    archive.pipe(output);
-    archive.glob('**/*', { cwd: path.join(exportsRoot) });
-    archive.finalize();
-    res.json({ export: `${exportsRoot}.zip` });
-    // res.sendFile(`${exportsRoot}.zip`);
   });
+}
+
+function sendExports(req, res, next) {
+  const data = currentExports[req.params.id];
+  if(!data) {
+    return res.status(404).end();
+  }
+  console.log(data);
+  // res.sendFile(fileName, options, error => { if(error) next(error); });
+}
+
+function pollExportProgress(req, res, next) {
+  const data = currentExports[req.params.id];
+  if(!data) {
+    return res.status(404).end();
+  }
+  res.json({ id: req.params.id, completed: data.completed, total: data.total, errors: data.errors });
+}
+
+function zipExports(dir) {
+  const output = fs.createWriteStream(`${dir}.zip`);
+  output.on('close', () => { fs.remove(dir); });
+  archive.pipe(output);
+  archive.glob('**/*', { cwd: path.join(dir) });
+  archive.finalize();
 }
 
 function mapCourseData(results) {
@@ -99,40 +132,3 @@ function toTimeString(dateString) {
 }
 
 init();
-
-/*
-server.get('/export/:tenant/:course/download.zip', function (req, res, next) {
-  var tenantId = req.params.tenant;
-  var courseId = req.params.course;
-  var userId = usermanager.getCurrentUser()._id;
-  // TODO not good specifying this here AND in plugins/output/adapt/export EXPORT_DIR
-  var zipDir = path.join(
-    configuration.tempDir,
-    configuration.getConfig('masterTenantID'),
-    Constants.Folders.Exports,
-    userId + '.zip'
-  );
-  // get the course name
-  app.contentmanager.getContentPlugin('course', function (error, plugin) {
-    if (error) return handleError(error, res);
-    plugin.retrieve({ _id:courseId }, {}, function(error, results) {
-      if (error) return handleError(error, res);
-      if (results.length !== 1) {
-        return handleError(new Error('Export: cannot find course (' + courseId + ')'), res);
-      }
-      fs.stat(zipDir, function(error, stat) {
-        if (error) return handleError(error, res);
-        var zipName = helpers.slugify(results[0].title,'export') + '.zip';
-        res.writeHead(200, {
-          'Content-Type': 'application/zip',
-          'Content-Length': stat.size,
-          'Content-disposition' : 'attachment; filename=' + zipName,
-          'Pragma' : 'no-cache',
-          'Expires' : '0'
-        });
-        fs.createReadStream(zipDir).pipe(res);
-      });
-    });
-  });
-});
-*/
